ff52e39a00bc1141e2c4694cc3b731eb4fd8b8c8
zemse <sohamzemse143@gmail.com>
2019-08-10 11:25:47 +0530

    remove redirect

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.use(function(req,res,next) {
   if(req.headers["x-forwarded-proto"] == "http") {
-    res.redirect("https://www.timeally.io" + req.url);
+    // res.redirect("https://www.betdeex.com" + req.url);
   } else {
     return next();
   }
