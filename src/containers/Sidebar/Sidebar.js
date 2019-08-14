c8a62b82a95bdb4ad065032fe27368fab0c6f77f
zemse <sohamzemse143@gmail.com>
2019-08-14 14:13:25 +0530

    sideber

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -18,7 +18,8 @@ const Sidebar = props => {
     }
 
     listOfCategories.push(
-      <Card key={categoryArray[categoryId]}>
+      <>
+      {/*<Card key={categoryArray[categoryId]}>
         <Accordion.Toggle as={Card.Header} variant="link" eventKey={categoryId}>
           {categoryArray[categoryId]}
         </Accordion.Toggle>
@@ -26,23 +27,58 @@ const Sidebar = props => {
           <Card>{listOfSubCategories}</Card>
         </Accordion.Collapse>
 
-      </Card>
-      
-      
-      // <li>
-      //   <a href="main.html"><img src="/img/icon/s1.png" alt="" />{categoryArray[categoryId]}</a>
-      // </li>
- 
-      
+      </Card>*/}{console.log(props)}
+      <li>
+        <a
+          onClick={() => props.history.push(`/explore/${categoryArray[categoryId].toLowerCase()}`)}
+          className={props.match.params.category === categoryArray[categoryId].toLowerCase()
+            ? 'menu-lef-act' : null}>
+          <img src={`/img/icon/${categoryArray[categoryId].toLowerCase()}.png`} alt="" />
+          {categoryArray[categoryId]}
+        </a>
+      </li>
+
+      </>
     );
   }
   console.log(props);
   if(props.location.pathname === '/') return null;
-  
+
   return (
-    <Accordion style={props.style}>
+    <>
+    {/*<Accordion style={props.style}>
       {listOfCategories}
-    </Accordion>
+    </Accordion>*/}
+    <section>
+          {/* LEFT SIDE NAVIGATION MENU */}
+          <div className="menu">
+            <ul>
+              {/* MAIN MENU */}
+              <li>
+                <a
+                  onClick={() => props.history.push(`/explore`)}
+                  className={props.location.pathname === '/explore' ? 'menu-lef-act' : null}><img src="/img/icon/s1.png" alt="" /> All</a>
+              </li>
+              {listOfCategories}
+              <li>
+                <a href="about.html"><img src="/img/icon/about.png" alt="" /> About</a>
+              </li>
+              <li>
+                <a href="contact.html"><img src="/img/icon/contact.png" alt="" /> Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          {/* RIGHT SIDE NAVIGATION MENU */}
+          {/* MOBILE MENU(This mobile menu show on 0px to 767px windows only) */}
+          <div className="mob-menu">
+            <span><i className="fa fa-bars" aria-hidden="true" /></span>
+          </div>
+          <div className="mob-close">
+            <span><i className="fa fa-times" aria-hidden="true" /></span>
+          </div>
+        </section>
+      {/* <Sidebar /> */}
+    </>
   );
 }
 
