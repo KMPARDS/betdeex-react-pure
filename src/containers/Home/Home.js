051779be0ade34c564e816e0f64d537a59770c98
bob <bob@gmail.com>
2019-08-14 12:17:54 +0530

    Caraousel slider add

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -155,164 +155,96 @@ const Home = () => (
           </div>
           </div>
       </section>
-        <section className="section singl-testimonial bg-1 edge top-left">
-          <div className="shape-wrapper"><img src="img/shps/abs-shp-1.svg" className="absolute top right h-100" alt="..." /></div>
-          <div className="container-wide bring-to-front">
-            <div className="row gap-y align-items-center">
-              <div className="col-12 col-lg-4 mx-auto">
-                <h2 style={{textAlign:'justify'}}>Propose a prediction event and get rewards</h2>
-                <p className="lead color-2">
-                  You can propose an Event on Swappers Wall. Events will be voted by community for likes. Top 10 Event proposals with maximum number of likes upto 25 August 2019 on Swappers Wall will be selected with the Reward of 1000 ES in liquid and 1000 ES in TimeAlly to proposers of each of these 10 Events.
-                </p>
-                <p>
-                </p>
-              </div>
-              <div className="col-10 col-lg-6 pr-0">
-                <div className="device browser" data-aos="fade-left">
-                  <div className="screen"><img src="img/screens/dash/1.png" alt="" className="img-responsive shadow" /></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="section">
-          <div className="shape-wrapper"><img src="img/shps/shape-2.svg" className="h-100 absolute left top" alt="..." /></div>
-          <div className="container-wide bring-to-front">
-            <div className="row gap-y align-items-center text-center text-lg-left">
-              <div className="col-10 col-lg-4 mx-auto order-md-2">
-                <h2>Predict on Events</h2>
-                <p className="lead color-2">Once there are events on BetDeEx, you can select your favourite event(s) and start prediction based on your skills</p>
-              </div>
-              <div className="col-12 col-lg-6 pl-0 order-md-1">
-                <div className="device browser" data-aos="fade-right">
-                  <div className="screen"><img src="img/screens/dash/2.png" alt="" className="img-responsive shadow" /></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="section singl-testimonial bg-1 edge top-left">
-          <div className="shape-wrapper"><img src="img/shps/abs-shp-1.svg" className="absolute top right h-100" alt="..." /></div>
-          <div className="container-wide bring-to-front">
-            <div className="row gap-y align-items-center">
-              <div className="col-12 col-lg-4 mx-auto">
-                <h2 style={{textAlign:'justify'}}>Use your Skills to Win</h2>
-                <p className="lead color-2">Once you have predicted the outcome and you got sucess, you can win. You can predict multiple events at the same time to win.</p>
-              </div>
-              <div className="col-10 col-lg-6 pr-0">
-                <div className="device browser" data-aos="fade-left">
-                  <div className="screen"><img src="img/screens/dash/3.png" alt="" className="img-responsive shadow" /></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        
         {/* Use Cases for Betdeex */}
         <section className="section bg-6 bg-6-gradient edge bottom-right">
           <div className="container bring-to-front">
-            <div className="section-heading text-center">
-              <p className="small bold badge badge-3">Use Cases for BetdeEx</p>
-              <h2>Use Cases for BetdeEx</h2>
-            </div>
-            <div className="row gap-y">
-              <div className="col-md-6">
-                <div className="rounded bg-1 shadow-box image-background contain off-left-background p-4 pl-6 pl-md-9" style={{backgroundImage: 'url(img/svg/political_prediction.png)'}}>
-                  <h3>Political Prediction</h3>
-                  <p className="color-2 mb-0">
-                    Now, predict on multiple political happenings across the globe and win on the outcomes
-                  </p>
+            <div id="demo" className="carousel slide" data-ride="carousel">
+              {/* Indicators */}
+              <ul className="carousel-indicators">
+                <li data-target="#demo" data-slide-to={0} className="active" />
+                <li data-target="#demo" data-slide-to={1} />
+                <li data-target="#demo" data-slide-to={2} />
+              </ul>
+              {/* The slideshow */}
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                <section className="section singl-testimonial bg-1 edge top-left">
+                    <div className="shape-wrapper"><img src="img/shps/abs-shp-1.svg" className="absolute top right h-100" alt="..." /></div>
+                    <div className="container-wide bring-to-front">
+                      <div className="row gap-y align-items-center">
+                        <div className="col-12 col-lg-7 mx-auto">
+                          <h2 style={{textAlign:'justify'}}>Propose a prediction event and get rewards</h2>
+                          <p className="lead color-2">
+                            You can propose an Event on Swappers Wall. Events will be voted by community for likes. Top 10 Event proposals with maximum number of likes upto 25 August 2019 on Swappers Wall will be selected with the Reward of 1000 ES in liquid and 1000 ES in TimeAlly to proposers of each of these 10 Events.
+                          </p>
+                          <p>
+                          </p>
+                        </div>
+                        <div className="col-10 col-lg-5 pr-0">
+                          <div className="device browser" data-aos="fade-left">
+                            <div className="screen"><img src="img/screens/dash/1.png" alt="" className="img-responsive shadow" /></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div className="carousel-item">
+                <section className="section singl-testimonial bg-1 edge top-left">
+                  <div className="shape-wrapper"><img src="img/shps/abs-shp-1.svg" className="absolute top right h-100" alt="..." /></div>
+                  <div className="container-wide bring-to-front">
+                    <div className="row gap-y align-items-center">
+                      <div className="col-12 col-lg-6 mx-auto">
+                        <h2 style={{textAlign:'justify'}}>Predict on Events</h2>
+                        <p className="lead color-2">Once there are events on BetDeEx, you can select your favourite event(s) and start prediction based on your skills.</p>
+                      </div>
+                      <div className="col-10 col-lg-5 pr-0">
+                        <div className="device browser" data-aos="fade-left">
+                          <div className="screen"><img src="img/screens/dash/3.png" alt="" className="img-responsive shadow" /></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                </div>
+                <div className="carousel-item">
+                <section className="section singl-testimonial bg-1 edge top-left">
+                  <div className="shape-wrapper"><img src="img/shps/abs-shp-1.svg" className="absolute top right h-100" alt="..." /></div>
+                  <div className="container-wide bring-to-front">
+                    <div className="row gap-y align-items-center">
+                      <div className="col-12 col-lg-7 mx-auto">
+                        <h2 style={{textAlign:'justify'}}>Use your Skills to Win</h2>
+                        <p className="lead color-2">Once you have predicted the outcome and you got sucess, you can win. You can predict multiple events at the same time to win.</p>
+                      </div>
+                      <div className="col-10 col-lg-5 pr-0">
+                        <div className="device browser" data-aos="fade-left">
+                          <div className="screen"><img src="img/screens/dash/3.png" alt="" className="img-responsive shadow" /></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
                 </div>
               </div>
-              <div className="col-md-6">
-                <div className="rounded bg-1 shadow-box image-background contain off-left-background p-4 pl-6 pl-md-9" style={{backgroundImage: 'url(img/svg/sport_prediction.png)'}}>
-                  <h3>Sports Prediction</h3>
-                  <p className="color-2 mb-0">
-                    Get you favourite sports and team, predict the outcomes of matches and sports events to win big
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="rounded bg-1 shadow-box image-background contain off-left-background p-4 pl-6 pl-md-9" style={{backgroundImage: 'url(img/svg/movie_prediction.png)'}}>
-                  <h3>Movies Prediction</h3>
-                  <p className="color-2 mb-0">
-                    Predict the future outcome of your favourite movies choosing the events from the list.
-                    Also predict on multiple events of Entertainment Industry.
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="rounded bg-1 shadow-box image-background contain off-left-background p-4 pl-6 pl-md-9" style={{backgroundImage: 'url(img/svg/events_prediction.png)'}}>
-                  <h3>Events Prediction</h3>
-                  <p className="color-2 mb-0">Predict on the outcomes of multiple events
-                    across the globe on Tech, Science, Space Exploration, Launches, Products and many more.</p>
-                </div>
-              </div>
+              {/* Left and right controls */}
+              <a className="carousel-control-prev" style={{color:'#000'}} href="#demo" data-slide="prev">
+                <span className="carousel-control-prev-icon" />
+              </a>
+              <a className="carousel-control-next" href="#demo" data-slide="next">
+                <span className="carousel-control-next-icon" />
+              </a>
             </div>
           </div>
         </section>
         {/*Currencies of Betdeex */}
-        <section className="section bg-6">
-          <div className="container bring-to-front py-0">
-            <div className="section-heading text-center">
-              <p className="small bold badge badge-3">Currencies of Betdeex</p>
-              <h2>Era Swap (ES) as Utility on BetdeEx</h2>
-            </div>
-            <div className="shadow bg-1 p-5 rounded">
-              <div className="row gap-y align-items-center text-center text-lg-left">
-                <div className="col-12 col-md-12 py-4 px-5 b-md-r">
-                  <img src="img/A-05.png" />
-                  <a href="javascript:;" className="mt-4 color-3 d-flex align-items-center">
-                    <h4 className="mr-3">Eraswap (ES)</h4>
-                    <i className="fas fa-long-arrow-alt-right" />
-                  </a>
-                  <p className="mt-4">Era Swap Token or ES is a decentralized utility token currently based on
-                    ethereum blockchain (ERC20) which will be used on multiple platforms for exchange of services (P2P),
-                    avail discounts, get rewards and many more utility.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+      
         {/* Benefits of Betdeex*/}
-        <section id="features" className="section">
-          <div className="container">
-            <div className="section-heading mb-6 text-center">
-              <h5 className="accent bold small text-uppercase">Benefits of BetdeEx</h5>
-              <h2>Benefits of BetdeEx</h2>
-            </div>
-            <div className="row gap-y text-center text-md-left">
-              <div className="col-md-3 py-4 rounded shadow-hover">
-                <i className="pe pe-7s-credit pe-3x accent" />
-                <h5 className="bold">Minimal Charges</h5>
-                <p className>BetdeEx charges minimal fee of 2% from winners on the winning amount.</p>
-              </div>
-              <div className="col-md-3 py-4 rounded shadow-hover">
-                <i className="pe pe-7s-add-user pe-3x accent" />
-                <h5 className="bold">User-driven Platform</h5>
-                <p className>BetdeEx is a user driven platform where users use their skills to predict outcomes and win.</p>
-              </div>
-              <div className="col-md-3 py-4 rounded shadow-hover">
-                <i className="pe pe-7s-next-2 pe-3x accent" />
-                <h5 className="bold">Ease of Access</h5>
-                <p className>BetdeEx is user friendly and any one can access from anywhere, anytime.</p>
-              </div>
-              <div className="col-md-3 py-4 rounded shadow-hover">
-                <i className="pe pe-7s-global pe-3x accent" />
-                <h5 className="bold">Anyone can Play</h5>
-                <p className>Any one can predict events on BetdeEx and win big.</p>
-              </div>
-            </div>
-          </div>
-          <div className="section-heading text-center">
-            <button disabled className="btn btn-rounded btn-xl btn-accent text-center">Start now by proposing a prediction event on Swappers Wall{/*Predict Now with BetdeEx*/}</button>
-          </div>
-        </section>
+       <hr></hr>
         {/*Join The Betdeex Ecosystem*/}
-        <section className="section bg-6 b-b edge">
+        <section className="section bg-6 b-b edge" style={{zIndex:'1111111'}}>
           <div className="container">
             <div className="section-heading text-center">
-              <h2>Join The  <span className="bold">Era Swap Ecosystem</span></h2>
+              <h2>Join The  <span className="bold" style={{fontSize:'48px'}}>Era Swap Ecosystem</span></h2>
               <p className="color-2 lead">Era Swap Ecosystem includes interlinked multiple platforms where Era Swap Tokens can
                 be utilized for exchange of services, availing discounts, getting rewards and other utilities. Era Swap Team will identify and help build more such platforms
                 in future which are built for the community and willing to incorporate ES reward system.</p>
@@ -386,71 +318,7 @@ const Home = () => (
               </div>
             </div>
           </div>
-        </section>
-        {/* Join the Community */}
-        <section className="section ">
-          <div className="container bring-to-front">
-            <div className="row gap-y ">
-              <div className="col-md-6 ml-md-auto text-center text-md-left">
-                <p className="badge badge-3 text-uppercase">Join the Community</p>
-                <h2>Join the Community </h2>
-                <hr />
-                <div className="rounded shadow-box bg-1 mt-3">
-                  <div className="media  px-3">
-                    <i className="pe pe-7s-chat pe-3x accent mr-3 mt-3" />
-                    <div className="media-body my-3 pl-3 b-l">
-                      <p className="bold my-0">Join the Conversation</p>
-                      <p className="my-0 color-2">BetdeEx platform is a decentralised platform for prediction on outcomes of multiple events in the
-                        domain of Politics, Sports, Movies, Current Affairs, Science, Tech, Space, products and many more.
-                        We welcome users to join our community to grow and prosper together.</p>
-                      <p>
-                        {/* <a href="" class="btn btn-rounded btn-outline-dark">Propose a prediction event <i class="fas fa-long-arrow-alt-right"></i></a> */}
-                        <button disabled className="btn btn-rounded btn-outline-dark">Propose a prediction event <i className="fas fa-long-arrow-alt-right" /></button>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="rounded shadow-box bg-1 mt-3">
-                  <div className="media  px-3">
-                    <i className="pe pe-7s-settings pe-3x accent mr-3  mt-3" />
-                    <div className="media-body my-3 pl-3 b-l">
-                      <p className="bold my-0">For Query</p>
-                      <p>
-                        {/* <a href="" class="btn btn-rounded btn-outline-dark">View Stack Exchange <i class="fas fa-long-arrow-alt-right"></i></a> */}
-                        <button disabled className="btn btn-rounded btn-outline-dark">View Stack Exchange <i className="fas fa-long-arrow-alt-right" /></button>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                {/* <div class="rounded shadow-box bg-1 mt-3">
-                        <div class="media  px-3">
-                           <i class="pe pe-7s-info pe-3x accent mr-3 mt-3"></i>
-                           <div class="media-body my-3 pl-3 b-l">
-                              <p class="bold my-0">Suggestions for Events</p>
-
-							  <p><a href="mailto:support@betdeex.com" class="btn btn-rounded btn-outline-dark">Connect through Email <i class="fas fa-long-arrow-alt-right"></i></a></p>
-                           </div>
-                        </div>
-                     </div> */}
-                <div className="rounded shadow-box bg-1 mt-3">
-                  <div className="media align-items-center px-3">
-                    <i className="pe pe-7s-like2 pe-3x accent mr-3" />
-                    <div className="media-body my-3 pl-3 b-l">
-                      <p className="bold my-0">Find BetdeEx Around the Web</p>
-                      <hr className="my-3" />
-                      <nav className="nav social-icons  mt-4">
-                        <a href="https://www.facebook.com/pages/BetDeEx/808455639538643" className="mr-3 color-2"><i className="fab fa-facebook-f" style={{fontSize: '2rem'}} /></a>
-                        {/* <a href="#" class="mr-3 font-regular color-2"><i class="fab fa-twitter"></i></a> */}
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        </section>       
         {/* ./CTA - Create Account */}
         <section className="section bg-6 edge top-left b-b">
           <div className="container pt-5">
@@ -467,8 +335,7 @@ const Home = () => (
               </div>                 
             </div>
           </div>
-        </section>
-      
+        </section>      
         {/* ./Footer - Simple */}
         <footer className="site-footer section bg-1 color-1 block bg-1">
           <div className="container py-4">
