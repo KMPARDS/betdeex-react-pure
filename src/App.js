6ff40a04305dd2ab909d80ddadde175e04e54e78
yogiraj <yogiraj77956@gmail.com>
2019-09-07 11:40:43 +0530

    Loaded add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux'; // this is for accessing the store\
-import { Row, Col } from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import {
+  connect
+} from 'react-redux'; // this is for accessing the store\
+import {
+  Row,
+  Col
+} from 'react-bootstrap';
 
 import Navbar from './containers/Navbar/Navbar';
 import Sidebar from './containers/Sidebar/Sidebar';
@@ -19,31 +28,39 @@ import provider from './ethereum/provider';
 import betdeexInstance from './ethereum/betdeexInstance';
 
 import './App.css';
-import { esContract, betdeex, categoryArray, subCategoryArray, network } from './env.js';
+import {
+  esContract,
+  betdeex,
+  categoryArray,
+  subCategoryArray,
+  network
+} from './env.js';
 
 const ethers = require('ethers');
 
 // one app login
-window.onload = function(){
-  !window.opener || window.opener.postMessage("loaded","*");
+window.onload = function () {
+  !window.opener || window.opener.postMessage("loaded", "*");
 
-  // document.getElementById('start-loader').style.display = 'none';
-  // const element = document.getElementById('start-loader');
-  // element.parentElement.removeChild(element);
+  document.getElementById('start-loader').style.display = 'none';
+  const element = document.getElementById('start-loader');
+  element.parentElement.removeChild(element);
 }
-window.addEventListener('message', function(e) {
-  setTimeout(() => window.ProcessParentMessage_2(e.data), 0);
-} , false);
+
+window.addEventListener('message', function (e) {
+  setTimeout(() => window.ProcessParentMessage_2 || window.ProcessParentMessage_2(e.data), 0);
+}, false);
 
 function App(props) {
 
-  document.getElementById('site-loader').style.display = 'none';
-
   window.ProcessParentMessage_2 = message => {
-    if(message.substring){
-      if(message.substring(0,2) == "0x"){
+    if (message.substring) {
+      if (message.substring(0, 2) == "0x") {
         // wallet = new ethers.Wallet(message);
-        props.dispatch({ type: 'LOAD-WALLET-INSTANCE', payload: new ethers.Wallet(message, provider) });
+        props.dispatch({
+          type: 'LOAD-WALLET-INSTANCE',
+          payload: new ethers.Wallet(message, provider)
+        });
         window.zHistory.push('/explore');
       }
     }
@@ -58,74 +75,150 @@ function App(props) {
   // },0);
 
   // if redux store is empty then try to copy the local storage if there is any data.
-  if(Object.entries(props.store.betsMapping).length === 0) {
+  if (Object.entries(props.store.betsMapping).length === 0) {
     const storedBetsMapping = JSON.parse(localStorage.getItem('betdeex-betsMapping') || '{}');
-    if(Object.entries(storedBetsMapping).length > 0) {
-      props.dispatch({ type: 'LOAD-BETS-MAPPING-FROM-LOCALSTORAGE', payload: storedBetsMapping });
+    if (Object.entries(storedBetsMapping).length > 0) {
+      props.dispatch({
+        type: 'LOAD-BETS-MAPPING-FROM-LOCALSTORAGE',
+        payload: storedBetsMapping
+      });
     }
   }
 
   // load es instance
-  if(Object.entries(props.store.esInstance).length === 0) {
+  if (Object.entries(props.store.esInstance).length === 0) {
     console.log(provider, new ethers.providers.InfuraProvider(network));
-    props.dispatch({ type: 'LOAD-ES-INSTANCE', payload: new ethers.Contract(esContract.address, esContract.abi, provider) });
+    props.dispatch({
+      type: 'LOAD-ES-INSTANCE',
+      payload: new ethers.Contract(esContract.address, esContract.abi, provider)
+    });
   }
 
   // load betdeex instance
-  if(Object.entries(props.store.esInstance).length === 0) {
-    props.dispatch({ type: 'LOAD-BETDEEX-INSTANCE', payload: new ethers.Contract(betdeex.address, betdeex.abi, provider) });
+  if (Object.entries(props.store.esInstance).length === 0) {
+    props.dispatch({
+      type: 'LOAD-BETDEEX-INSTANCE',
+      payload: new ethers.Contract(betdeex.address, betdeex.abi, provider)
+    });
   }
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Row>
-          {/*<Col style={{margin: '15px 0 0 15px'}}>
-          {console.log}
-            <Sidebar />
-          </Col>*/}
-          <Col style={{margin: false ? '15px 15px 0 0' : '0'}}>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/create-wallet" exact component={CreateWallet} />
-              <Route path="/load-wallet" component={LoadWallet} />
-              <Route path="/user" exact component={User} />
-              <Route path="/user/history" exact component={History} />
-              <Route path="/manager-panel" exact component={ManagerPanel} />
-              <Route path="/logout" exact component={Logout} />
-              <Route path="/bet/:address" exact component={BetView} />
-              <Route path="/explore" exact component={BetsList} />
-              <Route path="/explore/:category" exact render={props => {
-                let categoryWordArray = [];
-                for(const categoryWord of props.match.params.category.split('-')) {
-                  categoryWordArray.push(categoryWord.charAt(0).toUpperCase() + categoryWord.slice(1));
-                }
-                const categoryId = categoryArray.indexOf(categoryWordArray.join(' '));
+  return ( <
+    BrowserRouter >
+    <
+    div className = "App" >
+    <
+    Navbar / >
+    <
+    Row > {
+      /*<Col style={{margin: '15px 0 0 15px'}}>
+                {console.log}
+                  <Sidebar />
+                </Col>*/
+    } <
+    Col style = {
+      {
+        margin: false ? '15px 15px 0 0' : '0'
+      }
+    } >
+    <
+    Switch >
+    <
+    Route path = "/"
+    exact component = {
+      Home
+    }
+    /> <
+    Route path = "/create-wallet"
+    exact component = {
+      CreateWallet
+    }
+    /> <
+    Route path = "/load-wallet"
+    component = {
+      LoadWallet
+    }
+    /> <
+    Route path = "/user"
+    exact component = {
+      User
+    }
+    /> <
+    Route path = "/user/history"
+    exact component = {
+      History
+    }
+    /> <
+    Route path = "/manager-panel"
+    exact component = {
+      ManagerPanel
+    }
+    /> <
+    Route path = "/logout"
+    exact component = {
+      Logout
+    }
+    /> <
+    Route path = "/bet/:address"
+    exact component = {
+      BetView
+    }
+    /> <
+    Route path = "/explore"
+    exact component = {
+      BetsList
+    }
+    /> <
+    Route path = "/explore/:category"
+    exact render = {
+      props => {
+        let categoryWordArray = [];
+        for (const categoryWord of props.match.params.category.split('-')) {
+          categoryWordArray.push(categoryWord.charAt(0).toUpperCase() + categoryWord.slice(1));
+        }
+        const categoryId = categoryArray.indexOf(categoryWordArray.join(' '));
 
-                return <BetsList categoryId={categoryId} />;
-              }} />
-              <Route path="/explore/:category/:subCategory" exact render={props => {
-                let categoryWordArray = [];
-                for(const categoryWord of props.match.params.category.split('-')) {
-                  categoryWordArray.push(categoryWord.charAt(0).toUpperCase() + categoryWord.slice(1));
-                }
-                const categoryId = categoryArray.indexOf(categoryWordArray.join(' '));
+        return <BetsList categoryId = {
+          categoryId
+        }
+        />;
+      }
+    }
+    /> <
+    Route path = "/explore/:category/:subCategory"
+    exact render = {
+      props => {
+        let categoryWordArray = [];
+        for (const categoryWord of props.match.params.category.split('-')) {
+          categoryWordArray.push(categoryWord.charAt(0).toUpperCase() + categoryWord.slice(1));
+        }
+        const categoryId = categoryArray.indexOf(categoryWordArray.join(' '));
 
-                let subCategoryWordArray = [];
-                for(const subCategoryWord of props.match.params.subCategory.split('-')) {
-                  subCategoryWordArray.push(subCategoryWord.charAt(0).toUpperCase() + subCategoryWord.slice(1))
-                }
-                const subCategoryId = subCategoryArray[categoryId].indexOf(subCategoryWordArray.join(' '));
-                return <BetsList categoryId={categoryId} subCategoryId={subCategoryId} />;
-              }} />
-            </Switch>
-          </Col>
-        </Row>
-      </div>
-    </BrowserRouter>
+        let subCategoryWordArray = [];
+        for (const subCategoryWord of props.match.params.subCategory.split('-')) {
+          subCategoryWordArray.push(subCategoryWord.charAt(0).toUpperCase() + subCategoryWord.slice(1))
+        }
+        const subCategoryId = subCategoryArray[categoryId].indexOf(subCategoryWordArray.join(' '));
+        return <BetsList categoryId = {
+          categoryId
+        }
+        subCategoryId = {
+          subCategoryId
+        }
+        />;
+      }
+    }
+    /> < /
+    Switch > <
+    /Col> < /
+    Row > <
+    /div> < /
+    BrowserRouter >
   );
 }
 
-export default connect(state => {return{store: state}})(App); // this is for accessing the store
+export default connect(state => {
+  return {
+    store: state
+  }
+})(App); // this is for accessing the store
 //export default App;
