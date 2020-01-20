193a7dce9fc6cf60445051ae4dfb3227f90c153c
zemse <sohamzemse143@gmail.com>
2020-01-20 23:12:50 +0530

    add latest infographs

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -37,20 +37,15 @@ class Home extends Component {
           <div className="container-fluid" style={{'margin-top':'40px', padding: '0'}}>
               <Carousel infiniteLoop autoPlay emulateTouch={true} showArrows={false} >
                 <div >
-                  <img src="/img/BetDeEx_Infographic_01.jpg"  />
-                  <button className="legend" onClick={() => this.props.history.push('/bet/0x8F7f501f399323239C61449bE99b4ee9A02E2E4e')}>Click here to go to the event</button>
+                  <img src="/img/Betdeex-Wall-Post-01.jpg"  />
+                </div>
+                <div >
+                  <img src="/img/asian-wrestling.jpg"  />
+                  <button className="legend" onClick={() => this.props.history.push('/bet/0x4002c58993f2af1f16768fa0525f2e510aead33b')}>Click here to go to the event</button>
                 </div>
                 <div>
-                  <img src="/img/BetDeEx_Infographic_02.jpg" />
-                  <button className="legend" onClick={() => this.props.history.push('/bet/0x8F7f501f399323239C61449bE99b4ee9A02E2E4e')}>Click here to go to the event</button>
-                </div>
-                <div>
-                  <img src="/img/BetDeEx_Infographic_03.jpg" />
-                  <button className="legend" onClick={() => this.props.history.push('/bet/0x8F7f501f399323239C61449bE99b4ee9A02E2E4e')}>Click here to go to the event</button>
-                </div>
-                <div>
-                  <img src="/img/BetDeEx_Infographic_04.jpg" />
-                  <button className="last-img legend" onClick={() => this.props.history.push('/bet/0x8F7f501f399323239C61449bE99b4ee9A02E2E4e')}>Click here to go to the event</button>
+                  <img src="/img/ruling-party.jpg" />
+                  <button className="legend" onClick={() => this.props.history.push('/bet/0xcb07fcabbf589621e6b0d4e6d507aa3919e23c49')}>Click here to go to the event</button>
                 </div>
               </Carousel>
           </div>
