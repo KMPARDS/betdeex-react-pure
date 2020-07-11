a0d18e67599b5fad8ccab4af3f79cd4a83b3d557
smore0301 <06sdm666@gmail.com>
2020-07-11 18:22:26 +0530

    2 ternms & condition pdf added  11 july

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -367,13 +367,15 @@ class Home extends Component {
                   <nav className="nav flex-column">
                     <a className="nav-item py-2 color-1" href="/pdf/betdeex-user-guide.pdf" target="_blank"><span style={{color:'#848080'}}>User Guide</span></a>
                     <a className="nav-item py-2 color-1" onClick={() => this.props.history.push('/results')} style={{cursor:'pointer'}} target="_blank"><span style={{color:'#848080'}}>Results</span></a>
-                    <a className="nav-item py-2 color-1" href="/pdf/t&c.pdf" target="_blank"><span style={{color:'#848080'}}>Terms &amp; Conditions</span></a>
+                    <a className="nav-item py-2 color-1" href="https://eraswaptoken.io/pdf/eraswap-terms-conditions.pdf" target="_blank"><span style={{color:'#848080'}}>Era Swap Terms &amp; Conditions</span></a>
+                    <a className="nav-item py-2 color-1" href="/pdf/betdeex-terms-conditions.pdf" target="_blank"><span style={{color:'#848080'}}>Terms &amp; Conditions</span></a>
                     <a className="nav-item py-2 color-1" href="/pdf/privacy.pdf" target="_blank"><span style={{color:'#848080'}}>Privacy Policy</span></a>
-                    <a className="nav-item py-2 color-1" href="/pdf/eraswap_whitepaper.pdf" target="_blank"><span style={{color:'#848080'}}>Eraswap white paper</span></a>
+      
                   </nav>
                 </div>
                 <div className="col-md-4">
                   <nav className="nav flex-column">
+                    <a className="nav-item py-2 color-1" href="/pdf/eraswap_whitepaper.pdf" target="_blank"><span style={{color:'#848080'}}>Eraswap white paper</span></a>
                     <a className="nav-item py-2 color-1" href="https://etherscan.io/address/0x42225682113E6Ed3616B36B4A72BbaE376041D7c#code" target="_blank"><span style={{color:'#848080'}}>BetDeEx DApp Smart Contract</span></a>
                     <a className="nav-item py-2 color-1" href="https://eraswaptoken.io/era-swap-howey-test-letter-august7-2018.php" target="_blank"><span style={{color:'#848080'}}>Howey Test</span></a>
                     <a className="nav-item py-2 color-1" href="https://coinmarketcap.com/currencies/era-swap/" target="_blank"><span style={{color:'#848080'}}>CoinMarketCap</span></a>
